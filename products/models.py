a7bbf699d121876d0744c22052d540826908686d
charliebrown1221 <chaimbrown1@yahoo.com>
2022-05-24 16:23:13 -0400

    created serializer

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    title = CharField(max_length=255)
-    description = CharField
-    price = DecimalField
-    inventory_quantity = IntegerField
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
